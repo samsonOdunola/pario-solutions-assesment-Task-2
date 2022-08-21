a41e5e332e782a4ab411955974813af73cc90001
samsonOdunola <odunolasamson@yahoo.com>
2022-08-21 13:30:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Task2 from "./Task2";
+function App() {
+  return (
+    <div className="App">
+      <Task2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Task2.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Table from "react-bootstrap/Table";
+import Loadingicon from "./components/spinner";
+const Task2 = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [headLines, setHeadLines] = useState([]);
+  const [error, setError] = useState("");
+  const url = "https://covidnigeria.herokuapp.com/api";
+
+  //   create async function to fetch data from Api
+  const getData = async () => {
+    axios
+      .get(url)
+      .then((res, Error) => {
+        setData(res.data.data.states);
+        setHeadLines(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.code);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  return (
+    <div>
+      {loading ? (
+        <Loadingicon />
+      ) : error ? (
+        <h3 className="error">{error}</h3>
+      ) : (
+        <div>
+          <div className="headlines">
+            <h1>
+              Total Samples Tested:{" "}
+              {numberWithCommas(headLines.totalSamplesTested)}
+            </h1>
+            <h1>
+              Total Confirmed Cases:{" "}
+              {numberWithCommas(headLines.totalConfirmedCases)}
+            </h1>
+            <h1>
+              Total Active Cases: {numberWithCommas(headLines.totalActiveCases)}
+            </h1>
+            <h1>Dischared: {numberWithCommas(headLines.discharged)}</h1>
+            <h1>Deaths: {numberWithCommas(headLines.death)}</h1>
+          </div>
+          <Table className="table" striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>State</th>
+                <th>Confirmed Cases</th>
+                <th>Cases on Admission</th>
+                <th>Discharged</th>
+                <th>Death</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, index) => {
+                const {
+                  state,
+                  confirmedCases,
+                  casesOnAdmission,
+                  discharged,
+                  death,
+                  _id,
+                } = item;
+                return (
+                  <tr key={_id}>
+                    <td>{index + 1}</td>
+                    <td>{state}</td>
+                    <td>{numberWithCommas(confirmedCases)}</td>
+                    <td>{numberWithCommas(casesOnAdmission)}</td>
+                    <td>{numberWithCommas(casesOnAdmission)}</td>
+                    <td>{numberWithCommas(death)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Task2;
